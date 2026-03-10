9471db03164aab56f16e8a894d417b30cbfe09c8
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-03-10 15:26:07 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week06/SmartSpeaker.kt
@@ -0,0 +1,10 @@
+package oop_00000110698_MuhammadIrsalGinanjar.week06
+
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() { println("Speaker $name menyala.") }
+    override fun turnOff() { println("Speaker $name dimatikan.") }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
