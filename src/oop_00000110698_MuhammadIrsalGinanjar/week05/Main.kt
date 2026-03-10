3232a9ffed484fe4b25f2b60453e29eb2f901ef1
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-03-10 15:16:37 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000110698_MuhammadIrsalGinanjar/week05/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week05/Main.kt
@@ -3,26 +3,39 @@ package oop_00000110698_MuhammadIrsalGinanjar.week05
 fun main() {
     val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
     val admin1 = Admin(nama = "Bu Siti")
-
-    // Polymorphic Collection: List yang berisi tipe Parent, tapi isinya objek Anak
     val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
 
     println("=== AKTIVITAS PEGAWAI ===")
     for (pegawai in daftarPegawai) {
-        // Pemanggilan Runtime Polymorphism
         pegawai.bekerja()
-
-        // Smart Casting dengan is dan when
         when (pegawai) {
             is Dosen -> {
                 println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                pegawai.mengajar() // Smart cast! Tidak perlu manual casting
+                pegawai.mengajar()
             }
             is Admin -> {
                 println("=> Terdeteksi sebagai Admin")
                 pegawai.doAdminWork()
             }
         }
+        println()
+    }
+
+    println("=== TUGAS 1: MATH HELPER ===")
+    val math = MathHelper()
+    println("Luas Persegi (sisi 5): ${math.hitungLuas(5)}")
+    println("Luas Persegi Panjang (5 x 10): ${math.hitungLuas(5, 10)}")
+    println("Luas Lingkaran (jari 7.0): ${math.hitungLuas(7.0)}")
+    println()
+
+    println("=== TUGAS 2: SISTEM PEMBAYARAN ===")
+    val myEWallet = EWallet("Isal EWallet", 50000.0)
+    val myCreditCard = CreditCard("Isal CC", 100000.0)
+    val paymentMethods: List<PaymentMethod> = listOf(myEWallet, myCreditCard)
+
+    for (payment in paymentMethods) {
+        println("Memproses pembayaran Rp75000 untuk: ${payment.accountName}")
+        payment.processPayment(75000.0)
         println()
     }
 }
