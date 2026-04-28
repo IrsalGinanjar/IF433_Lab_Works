18d9849461cb83dc45c6b7c8339076da6d7d71f4
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 15:55:46 +0700

    week07: (task) create GameItem data class

--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/GameModels.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/GameModels.kt
@@ -1,9 +1,7 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week07
 
 enum class ItemRarity(val dropChance: Int) {
-    COMMON(70),
-    UNCOMMON(20),
-    RARE(6),
-    EPIC(3),
-    LEGENDARY(1)
+    COMMON(70), UNCOMMON(20), RARE(6), EPIC(3), LEGENDARY(1)
 }
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
