8b8782b9b4f932ad7494a4eb6985ef908444e965
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 15:57:53 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
@@ -1,13 +1,7 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week07
 
 fun main() {
-    println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    val uiMessage = when (response) {
-        is ApiResponse.Success -> "Tampilkan: ${response.data}"
-        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
-        ApiResponse.Loading -> "Tampilkan Spinner"
-    }
-    println(uiMessage)
+    println("\n=== TUGAS: RPG ENGINE ===")
+    GameManager.startGame()
+    GameManager.startGame()
 }
