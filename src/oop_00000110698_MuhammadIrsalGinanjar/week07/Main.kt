e4a0e6f1e14a0284eb7bee2ff372b2cc94bb1653
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 15:53:05 +0700

    week07: implement data class for structural equality

--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
@@ -1,16 +1,10 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week07
 
 fun main() {
-    println("=== TEST SINGLETON ===")
-    DatabaseManager.connect()
-
-    println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient()
-    client.connect()
-
-    println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1) // Akan mencetak memori hash
-    println("Sama? ${reg1 == reg2}") // False
+    // ... (Test sebelumnya diabaikan saja biar hemat tempat, tambahkan di bawahnya)
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1) // Otomatis readable format
+    println("Sama? ${data1 == data2}") // True (Structural Equality)
 }
--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/UsersModels.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/UsersModels.kt
@@ -1,3 +1,5 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week07
 
 class RegularUser(val name: String, val age: Int)
+
+data class DataUser(val name: String, val age: Int)
