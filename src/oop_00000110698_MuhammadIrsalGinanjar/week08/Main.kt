4099ad5bc3f27969362cbf6b17bb3e689c476e9e
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:02:35 +0700

    week08: create mixed data collection

--- src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
@@ -1,17 +1,14 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week08
 
 fun main() {
-    println("=== TEST SAFE CALLS & ELVIS ===")
-    val emptyOrder = Order(null, null)
-    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
-    println("Tujuan pengiriman: $destination")
+    // ... (Test sebelumnya biarkan tetap ada di atas ini) ...
 
-    println("\n=== TEST LET BLOCK ===")
-    val validOrder = Order(null, 250000)
-    val receipt = validOrder.totalPrice?.let { price ->
-        // Blok ini HANYA jalan jika totalPrice tidak null
-        val tax = price * 0.11
-        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
-    } ?: "Transaksi Invalid: Harga belum di-set!"
-    println(receipt)
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }
