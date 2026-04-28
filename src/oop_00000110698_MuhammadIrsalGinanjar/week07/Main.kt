3965669d1f9c901a05936065f0eaaeec985e5d36
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 15:58:06 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
@@ -3,5 +3,8 @@ package oop_00000110698_MuhammadIrsalGinanjar.week07
 fun main() {
     println("\n=== TUGAS: RPG ENGINE ===")
     GameManager.startGame()
-    GameManager.startGame()
+
+    println("\n=> Drop Chance Legendary: ${ItemRarity.LEGENDARY.dropChance}%")
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("=> Senjata dibuat: ${starterWeapon.item.name} | Damage: ${starterWeapon.item.damage} | Durability: ${starterWeapon.durability}")
 }
