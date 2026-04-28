3b3a73d02c68271f5889ca2630fd9d5f45fbe62c
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:02:45 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
@@ -1,7 +1,7 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week08
 
 fun main() {
-    // ... (Test sebelumnya biarkan tetap ada di atas ini) ...
+    // ... (Test sebelumnya) ...
 
     println("\n=== TEST SAFE CASTING ===")
     val mixedData: List<Any> = listOf(
@@ -11,4 +11,12 @@ fun main() {
         "Laptop",
         4500000.0
     )
+
+    for (item in mixedData) {
+        val text = item as? String
+        // Hanya cetak jika cast sukses (text tidak null)
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
