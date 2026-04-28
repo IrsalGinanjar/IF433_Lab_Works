c805f903d110b4316e3f4a8d495b8a669087536d
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:19:31 +0700

    week10: (task) populate coin repository

--- src/oop_00000110698_MuhammadIrsalGinanjar/week10/CryptoDashboard.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000110698_MuhammadIrsalGinanjar.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.15))
+    coinRepo.add(Coin("ETH", 2.5))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
