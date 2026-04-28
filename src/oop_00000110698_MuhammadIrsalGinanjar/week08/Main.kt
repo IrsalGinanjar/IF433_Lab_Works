4679ffed152b8e15f852b2f794935bdd7ec883a3
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:03:03 +0700

    week08: (trial) trigger NullPointerException with !!

--- src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
@@ -3,16 +3,12 @@ package oop_00000110698_MuhammadIrsalGinanjar.week08
 fun main() {
     // ... (Test sebelumnya) ...
 
-    println("\n=== TEST SAFE CASTING ===")
-    val mixedData: List<Any> = listOf("Smartphone", 1500000, UserProfile("Andi", null), "Laptop", 4500000.0)
-
-    for (item in mixedData) {
-        val text = item as? String
-        text?.let { println("Ditemukan teks: ${it.uppercase()}") }
+    println("\n=== TEST THE RED BUTTON (!!) ===")
+    val toxicData: String? = null
+    try {
+        // DANGEROUS: Memaksa compiler percaya data ini tidak null
+        val length = toxicData!!.length
+    } catch (e: NullPointerException) {
+        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
     }
-
-    val someObject: Any = 100 // Tipe aslinya Integer
-    // Coba cast ke String. Jika gagal (null), ganti dengan "Unknown String"
-    val safeString = someObject as? String ?: "Unknown String"
-    println("Hasil cast fallback: $safeString")
 }
