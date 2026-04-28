51117719758adc7a4d11382e56aa13275b68fbf1
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:12:56 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_00000110698_MuhammadIrsalGinanjar/week09/TradingDashboard.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week09/TradingDashboard.kt
@@ -26,4 +26,6 @@ fun main() {
 
     println("=== CRYPTO TRADING DASHBOARD ===")
     topPerformersString.forEach { println(it) }
+    worstPerformersString.forEach { println(it) }
+    println("\nUnique Pairs Traded: $uniquePairs")
 }
