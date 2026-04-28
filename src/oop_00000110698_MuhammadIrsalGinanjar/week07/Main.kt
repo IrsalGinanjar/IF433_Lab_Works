819cc1412df98ca7b3b4246149e7cd3c367bce5c
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 15:52:29 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
@@ -2,10 +2,15 @@ package oop_00000110698_MuhammadIrsalGinanjar.week07
 
 fun main() {
     println("=== TEST SINGLETON ===")
-    println("Status: ${DatabaseManager.connectionStatus}")
     DatabaseManager.connect()
 
     println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient() // Instansiasi lewat Factory
+    val client = NetworkClient.createClient()
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1) // Akan mencetak memori hash
+    println("Sama? ${reg1 == reg2}") // False
 }
---
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/UsersModels.kt
@@ -0,0 +1,3 @@
+package oop_00000110698_MuhammadIrsalGinanjar.week07
+
+class RegularUser(val name: String, val age: Int)
