d38cf3bfbd117d3e4ff413773d7207cdecf8b69e
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:20:01 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000110698_MuhammadIrsalGinanjar/week10/CryptoDashboard.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week10/CryptoDashboard.kt
@@ -2,8 +2,9 @@ package oop_00000110698_MuhammadIrsalGinanjar.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
-
     coinRepo.add(Coin("BTC", 0.15))
     coinRepo.add(Coin("ETH", 2.5))
     coinRepo.add(Coin("USDT", 1500.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
