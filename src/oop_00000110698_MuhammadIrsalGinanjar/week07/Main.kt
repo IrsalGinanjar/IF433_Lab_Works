c855e08eafd4c28d9e46ca143f950f1b268651b8
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 15:53:40 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week07/Main.kt
@@ -1,10 +1,12 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week07
 
 fun main() {
-    // ... (Test sebelumnya diabaikan saja biar hemat tempat, tambahkan di bawahnya)
-    println("\n=== TEST DATA CLASS ===")
     val data1 = DataUser("Alice", 22)
-    val data2 = DataUser("Alice", 22)
-    println(data1) // Otomatis readable format
-    println("Sama? ${data1 == data2}") // True (Structural Equality)
+
+    println("\n=== TEST COPY & DESTRUCTURING ===")
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    val (userName, userAge) = data1 // Destructuring Declaration
+    println("Destructured: $userName berumur $userAge")
 }
