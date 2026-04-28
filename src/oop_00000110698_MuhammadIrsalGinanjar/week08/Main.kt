d630cb733d8564690e00b1640b6e44ae07c6c94e
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:04:10 +0700

    week08: implement java interop using !!

--- src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
@@ -3,21 +3,9 @@ package oop_00000110698_MuhammadIrsalGinanjar.week08
 fun main() {
     // ... (Test sebelumnya) ...
 
-    println("\n=== TEST THE RED BUTTON (!!) ===")
-    val toxicData: String? = null
-    try {
-        val length = toxicData!!.length
-    } catch (e: NullPointerException) {
-        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
-    }
-
-    val apiResponse: Map<String, String?> = mapOf("status" to "200", "token" to null)
-    try {
-        // Alih-alih apiResponse["token"]!!
-        val token = requireNotNull(apiResponse["token"]) {
-            "CRITICAL EXCEPTION: Token otentikasi tidak ditemukan dari server!"
-        }
-    } catch (e: IllegalArgumentException) {
-        println(e.message) // Mencetak pesan custom kita, bukan crash buta
-    }
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    // Kita menekan tombol !! karena KITA TAHU implementasi Java-nya aman
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
