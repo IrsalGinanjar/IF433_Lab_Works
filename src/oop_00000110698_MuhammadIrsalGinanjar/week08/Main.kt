ed8beaccae4742272a30e46b1d9ecba584dff489
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:02:53 +0700

    week08: create fallback logic with as? and elvis

--- src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
@@ -4,19 +4,15 @@ fun main() {
     // ... (Test sebelumnya) ...
 
     println("\n=== TEST SAFE CASTING ===")
-    val mixedData: List<Any> = listOf(
-        "Smartphone",
-        1500000,
-        UserProfile("Andi", null),
-        "Laptop",
-        4500000.0
-    )
+    val mixedData: List<Any> = listOf("Smartphone", 1500000, UserProfile("Andi", null), "Laptop", 4500000.0)
 
     for (item in mixedData) {
         val text = item as? String
-        // Hanya cetak jika cast sukses (text tidak null)
-        text?.let {
-            println("Ditemukan teks: ${it.uppercase()}")
-        }
+        text?.let { println("Ditemukan teks: ${it.uppercase()}") }
     }
+
+    val someObject: Any = 100 // Tipe aslinya Integer
+    // Coba cast ke String. Jika gagal (null), ganti dengan "Unknown String"
+    val safeString = someObject as? String ?: "Unknown String"
+    println("Hasil cast fallback: $safeString")
 }
