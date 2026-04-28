4fb44a0edaa30f40099c5def2a8c4ca098b6392b
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:06:13 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000110698_MuhammadIrsalGinanjar.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
