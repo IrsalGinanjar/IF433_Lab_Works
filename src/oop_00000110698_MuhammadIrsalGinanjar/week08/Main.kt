307dc376e598e5fbf1cbe5cff73489908218377c
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-04-28 16:04:45 +0700

    week08: simulate unit test failure using !!

--- src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week08/Main.kt
@@ -1,11 +1,7 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week08
 
 fun main() {
-    // ... (Test sebelumnya) ...
+    // ... (Semua test sebelumnya) ...
 
-    println("\n=== TEST JAVA INTEROP ===")
-    val javaResponse = LegacyJavaAPI.fetchServerStatus()
-    // Kita menekan tombol !! karena KITA TAHU implementasi Java-nya aman
-    val statusLength = javaResponse!!.length
-    println("Status dari Java: $javaResponse (Length: $statusLength)")
+    runMockUnitTest()
 }
