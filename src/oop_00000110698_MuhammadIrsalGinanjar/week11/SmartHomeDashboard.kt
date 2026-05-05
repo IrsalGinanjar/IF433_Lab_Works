9876520fb26d9d17c0f8e14097155850ad4a780b
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-05-05 14:15:17 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000110698_MuhammadIrsalGinanjar/week11/SmartHomeDashboard.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week11/SmartHomeDashboard.kt
@@ -2,4 +2,12 @@ package oop_00000110698_MuhammadIrsalGinanjar.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    // 4. Konfigurasi Pencahayaan (apply & also)
+    SmartDevice("", "", false, 0).apply {
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also { homeDevices.add(it) }
 }
