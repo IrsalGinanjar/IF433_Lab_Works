d3a51d83f3d4c34fe8bf01741242843d0dca0ffa
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-05-05 14:20:31 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000110698_MuhammadIrsalGinanjar/week11/SmartHomeDashboard.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week11/SmartHomeDashboard.kt
@@ -3,11 +3,19 @@ package oop_00000110698_MuhammadIrsalGinanjar.week11
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
 
-    // 4. Konfigurasi Pencahayaan (apply & also)
     SmartDevice("", "", false, 0).apply {
         name = "Philips WiZ Living Room"
         category = "Lighting"
         isOnline = true
         powerLoad = 12
     }.also { homeDevices.add(it) }
+
+    // 5. Konfigurasi Keamanan (also & apply)
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
+        homeDevices.add(it)
+    }
 }
