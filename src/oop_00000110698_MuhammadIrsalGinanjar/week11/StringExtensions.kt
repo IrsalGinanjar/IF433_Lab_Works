e5e87aab02b46476502f61a086030f596df29aec
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-05-05 14:04:49 +0700

    week11: create nullable receiver extension

--- src/oop_00000110698_MuhammadIrsalGinanjar/week11/StringExtensions.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week11/StringExtensions.kt
@@ -1,11 +1,14 @@
 package oop_00000110698_MuhammadIrsalGinanjar.week11
 
-// Extension function dengan receiver 'this'
 fun String.addGreeting(): String {
     return "Hello, $this"
 }
 
-// Extension function dengan parameter
 fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
+}
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    // 'this' bisa bernilai null, jadi harus ditangani
+    return this == null || this.isEmpty()
 }
