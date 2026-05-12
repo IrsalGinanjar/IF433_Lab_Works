f1b4ce26e636de9da6eac5537cccd9f18415b966
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-05-12 13:39:28 +0700

    week12: handle Result using getOrElse and recover

--- src/oop_00000110698_MuhammadIrsalGinanjar/week12/RunCatchingExample.kt
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week12/RunCatchingExample.kt
@@ -2,7 +2,17 @@ package oop_00000110698_MuhammadIrsalGinanjar.week12
 
 fun main() {
     println("=== TEST RUNCATCHING ===")
+
+    // Checkpoint 6
     val result: Result<Int> = runCatching {
         "42X".toInt()
     }
+
+    // Checkpoint 7: Pattern getOrElse
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
+
+    // Checkpoint 7: Pattern recover
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
 }
