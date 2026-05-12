ec6ff5c70331c62f77c12d114e9de05cdf86f12b
Muhammad Irsal Ginanjar <mirsalginanjar@gmail.com>
2026-05-12 13:35:41 +0700

    week12: implement basic runCatching

---
+++ src/oop_00000110698_MuhammadIrsalGinanjar/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_00000110698_MuhammadIrsalGinanjar.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
